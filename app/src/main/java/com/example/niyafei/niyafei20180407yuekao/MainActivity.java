a193f96d638e7e58a5aba7eca740f462b576665c
19980311 <2661375687QQ@.com>
2018-04-10 10:51:16 +0800

    第一次提交

---
+++ app/src/main/java/com/example/niyafei/niyafei20180407yuekao/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.niyafei.niyafei20180407yuekao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
